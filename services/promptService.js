36342b6599698e50978e14c28a2ac1fbc7ab84ca
Narendra N. Kalekar <kalekar.narendra@gmail.com>
2024-01-24 14:56:14 +0530

    Update promptService.js

--- services/promptService.js
+++ services/promptService.js
@@ -1,15 +1,18 @@
 const constants = require("../common/constants");
 
-getCreateStoryPrompt = (type, genre) => {
-  return `create a ${type} story of genre ${genre} in ${constants.STORY_LENGTH} words without title`;
+getCreateStoryPrompt = (type, genre, country, people) => {
+  return `You are a famous story writer from country ${country}. 
+  You are popular for writing stories for ${people}. Now create a ${type} story of genre ${genre} in ${constants.STORY_LENGTH} words without title`;
 };
 
 getCreateTitlePrompt = (story) => {
   return `Given the story "${story}" create a title in ${constants.TITLE_LENGTH} words`;
 };
 
-getCreateTwistPrompt = (type, story) => {
-  return `Given the story ${story} create further ${type} story in ${constants.STORY_LENGTH} words without title`;
+getCreateTwistPrompt = (type, story, country, people) => {
+  return `You are a famous story writer from country ${country}. 
+  You are popular for writing stories for ${people}.
+  Given the story ${story} create further ${type} story in ${constants.STORY_LENGTH} words without title`;
 };
 
 module.exports = {
